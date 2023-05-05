559d47b73507aced93a2eae28696697d1c79b8fc
ombasaabuya <ombasabuya@gmail.com>
2023-05-05 09:52:00 +0300

    added src/Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+      <div>
+        <nav className="navbar"></nav>
+        <h1>The Abuya Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">New Blog </a>
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default Navbar;
+  
